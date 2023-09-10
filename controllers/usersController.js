b4476af298920be6469e79546711e276a0724fe4
Wais-MOF <wais.alizada2003@gmail.com>
2023-09-10 16:11:16 +0430

    users model created

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,17 @@
+const usersCollection = require('../models/usersModel');
 
-exports.usersPage = (req , res , next)=>{    
+
+exports.usersPage = (req , res , next)=>{   
+    usersCollection.create({
+        name:"zamir",
+        lastName:"haidari",
+        fatherName:"nasir",
+        occupation:"NTA",
+        accessLevel:2,
+    });
     res.render('index',{
         title: "users page",
         description:"users page is ready"
     })
 };
+
