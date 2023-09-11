a4ccae186f3d457a200518cbe83a55ddf1e3ddd1
Wais Alizada <56804806+wais865@users.noreply.github.com>
2023-09-11 22:47:22 +0430

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,13 +2,13 @@ const usersCollection = require('../models/usersModel');
 
 
 exports.usersPage = (req , res , next)=>{   
-    usersCollection.create({
-        name:"zamir",
-        lastName:"haidari",
-        fatherName:"nasir",
-        occupation:"NTA",
-        accessLevel:2,
-    });
+    // usersCollection.create({
+    //     name:"zamir",
+    //     lastName:"haidari",
+    //     fatherName:"nasir",
+    //     occupation:"NTA",
+    //     accessLevel:2,
+    // });
     res.render('index',{
         title: "users page",
         description:"users page is ready"
