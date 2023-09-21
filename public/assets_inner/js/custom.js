24265543e76a221d7145b318bcb46853fd6a3a6a
Wais-MOF <wais.alizada2003@gmail.com>
2023-09-21 08:23:42 +0430

    links
    local links added

--- public/assets_inner/js/custom.js
+++ public/assets_inner/js/custom.js
@@ -12,18 +12,18 @@ export function datatablesForm () {
           processing: true,
           serverSide: true,
           ajax: "/api/customers",  // API endpoint to fetch data
-            columns: [
-                { data: 'name' ,defaultContent : ""},
-                { data: 'father_name' ,defaultContent : ""},
-                { data: 'degree' ,defaultContent : ""},
-                { data: 'management' ,defaultContent : ""},
-                { data: 'document.command_date' ,defaultContent : ""},
-                { data: 'purpose' ,defaultContent : ""}
-            ],
-            dom: 'Bfrtip',  // Add this line to control where the buttons appear
-            buttons: [
-                'copy', 'excel', 'pdf'
-            ]
+          columns: [
+              { data: 'name' ,defaultContent : ""},
+              { data: 'father_name' ,defaultContent : ""},
+              { data: 'degree' ,defaultContent : ""},
+              { data: 'management' ,defaultContent : ""},
+              { data: 'document.command_date' ,defaultContent : ""},
+              { data: 'purpose' ,defaultContent : ""}
+          ],
+          dom: 'Bfrtip',  // Add this line to control where the buttons appear
+          buttons: [
+              'copy', 'excel', 'pdf'
+          ]
         });
         
         // Debouncing function for decreaseing server load on every key change
